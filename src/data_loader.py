e56390cd9ca61eef185fd4e2d544658099960e6e
WHahn664 <152346599+WHahn664@users.noreply.github.com>
2025-05-16 06:00:07 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -14,3 +14,4 @@ def split_data(X, y_raw, y_log):
     X_train, X_test, y_raw_train, y_raw_test = train_test_split(X, y_raw, test_size=0.2, random_state=42)
     _, _, y_log_train, y_log_test = train_test_split(X, y_log, test_size=0.2, random_state=42)
     return X_train, X_test, y_raw_train, y_raw_test, y_log_train, y_log_test
+
